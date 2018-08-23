259203200ac91a235592839de52e78fa7feedea8
Josh Wright <jwwrightcalpoly@gmail.com>
2018-08-23 13:20:26 -0700

    fixed npm playground

--- playground/index.ts
+++ playground/index.ts
@@ -5,12 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Component } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AutoSizeInputModule } from '../src';
+import { AutoSizeInputModule } from '../src/index';
 
 
 @Component({
   selector: 'app',
-  template: `<input type="text" AutoSizeInput style="font-weight: bold">`
+  template: `Test <input type="text" AutoSizeInput>`
 })
 class AppComponent {}
 
