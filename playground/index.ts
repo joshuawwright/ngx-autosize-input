0e73ba65eff420ff85d13ab13bf05a57c4d2b8e1
Josh Wright <jwwrightcalpoly@gmail.com>
2018-08-24 10:18:33 -0700

    updated peer dependencies and to version 2.2.1

--- playground/index.ts
+++ playground/index.ts
@@ -2,15 +2,14 @@
  * This is only for local test
  */
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { Component } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AutoSizeInputModule } from '../src/index';
 
 
 @Component({
-  selector: 'app',
-  template: `<input type="text" AutoSizeInput style="font-weight: bold">`
+  selector: 'app-playground',
+  template: `<input type="text" AutoSizeInput>`
 })
 class AppComponent {}
 
