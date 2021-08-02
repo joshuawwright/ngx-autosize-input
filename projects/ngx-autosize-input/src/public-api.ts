f878b448bdc028957f15e7328eda9f00183df418
Jamie Clarke <jamie@clarke.codes>
2021-08-02 10:55:22 +0100

    Expose AutoSizeInputDirective in public api

--- projects/ngx-autosize-input/src/public-api.ts
+++ projects/ngx-autosize-input/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of ngx-autosize-input
  */
 
+export * from './lib/auto-size-input.directive';
 export * from './lib/auto-size-input.options';
 export * from './lib/auto-size-input.module';
